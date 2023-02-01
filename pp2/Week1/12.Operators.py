cbd6791a3dad0edec645f60c50b93b9c1afb6ec1
Bayron6373 <124043007+Bayron6373@users.noreply.github.com>
2023-02-01 19:21:20 +0600

    Add files via upload

---
+++ pp2/Week1/12.Operators.py
@@ -0,0 +1,5 @@
+x = 32 
+print(2**4)
+print(2 >= 2)
+print(x > 10 and x < 40)
+print(x > 40 or x < 100)
---
+++ pp2/Week1/training.py
@@ -0,0 +1,6 @@
+txt = "I like Marvels \"Superheros\" a lot"
+print(txt)
+
+print(2**5)
+print(25%5)
+print(25 >= 25)
