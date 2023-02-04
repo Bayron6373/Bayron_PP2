8a2744e7f52f80415a871cbc64995495cedb3fed
Bayron6373 <124043007+Bayron6373@users.noreply.github.com>
2023-02-04 22:28:48 +0600

    Add files via upload

---
+++ Week2/4.Arrays.py
@@ -0,0 +1,14 @@
+#Try it yourself
+footballplayers = ["Kaka","Ozil","Benzema"]
+x = footballplayers[0]
+print(x)
+cars = ["BMW","AUDI","TOYOTA"]
+cars[1] = "MERSEDES"
+print(cars)
+print(len(cars))
+for x in cars:
+    print(x)
+cars.append("NISSAN")
+print(cars)
+cars.pop(2)
+print(cars)
