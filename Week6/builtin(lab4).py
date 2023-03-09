ae58a99e808c6e35423f0bce8afeb8d03a13867b
Bayron6373 <124043007+Bayron6373@users.noreply.github.com>
2023-03-09 12:08:21 +0600

    Delete builtin(lab4).py

--- Week6/builtin(lab4).py
+++
@@ -1,9 +0,0 @@
-import time
-import math
-
-def delayed_sqrt(number, milliseconds):
-    time.sleep(milliseconds/1000)
-    return math.sqrt(number)
-
-result = delayed_sqrt(input(), input())
-print(result)
