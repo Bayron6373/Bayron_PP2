684c2b2ea40637a61a3ccfc67e7e864e3e7daaf8
Bayron6373 <124043007+Bayron6373@users.noreply.github.com>
2023-03-09 12:09:32 +0600

    Delete dirandfiles(lab5).py

--- Week6/dirandfiles(lab5).py
+++
@@ -1,9 +0,0 @@
-my_list = ["apple", "banana", "cherry", "date"]
-filename = "path/to/file.txt"
-
-with open(filename, 'w') as f:
-    for item in my_list:
-        f.write("%s\n" % item)
-
-print(f"The list has been written to {filename}.")
-
