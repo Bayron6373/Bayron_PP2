8a39c09c54287a6a14b3cd3ca8455e8491246242
Bayron6373 <124043007+Bayron6373@users.noreply.github.com>
2023-03-09 12:09:23 +0600

    Delete dirandfiles(lab4).py

--- Week6/dirandfiles(lab4).py
+++
@@ -1,8 +0,0 @@
-filename = "path/to/file.txt"
-num_lines = 0
-
-with open(filename, 'r') as f:
-    for line in f:
-        num_lines += 1
-
-print(f"The number of lines in {filename} is {num_lines}.")
